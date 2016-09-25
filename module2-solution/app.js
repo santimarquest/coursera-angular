23ad77101aeb076ac7aa63d8429744386bd64587
DESKTOP-9KGNBCR\SANTI <santimarquest@hotmail.com>
2016-09-25 13:20:49 +0200

    index

---
+++ module2-solution/app.js
@@ -0,0 +1,62 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+  var tobuy = this;
+
+  tobuy.itemName = "";
+  tobuy.itemQuantity = "";
+
+  itemAdder.addItem = function () {
+    ShoppingListCheckOffService.addItem(tobuy.itemName, tobuy.itemQuantity);
+  }
+}
+
+
+//////////////////////////////////////////////////////////////////
+
+
+
+ShoppingListShowController.$inject = ['ShoppingListService'];
+function ShoppingListShowController(ShoppingListService) {
+  var showList = this;
+
+  showList.items = ShoppingListService.getItems();
+
+  showList.removeItem = function (itemIndex) {
+    ShoppingListService.removeItem(itemIndex);
+  };
+}
+
+
+function ShoppingListService() {
+  var service = this;
+
+  // List of shopping items
+  var items = [];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    items.push(item);
+  };
+
+  service.removeItem = function (itemIdex) {
+    items.splice(itemIdex, 1);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+}
+
+})();
